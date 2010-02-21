f119ad8c0ec7788f521f23db5c9fc051fe40614f
ericaro@gmail.com <ericaro@gmail.com@55289204-0ce5-11df-9e6e-d17b0969bdea>
2010-02-21 23:26:37 +0000

    testsimple was no longer that simple removed it
    git-svn-id: https://diezel.googlecode.com/svn/trunk@31 55289204-0ce5-11df-9e6e-d17b0969bdea

--- core/src/test/java/net/ericaro/diezel/core/builder/BuilderParserTest.java
+++ core/src/test/java/net/ericaro/diezel/core/builder/BuilderParserTest.java
@@ -30,27 +30,8 @@ public class BuilderParserTest extends TestCase {
 		}
 	}
 	
-	public void testSimple() throws Exception {
-		
-		String s = Builder.parse(Simple.class).toString();
-		System.out.println(s);
-		String gold = "dsl: t | b\n" + 
-				"package: net.ericaro.diezel.core.builder\n" + 
-				"type: net.ericaro.diezel.core.builder.BuilderParserTest.Simple<T, V>\n" + 
-				"transition:\n" + 
-				"	name: t1\n" + 
-				"	alias: t\n" + 
-				"	throws: java.lang.Exception\n" + 
-				"	args: java.lang.String\n" + 
-				"\n" + 
-				"";
-		System.out.println(gold);
-		assertEquals(gold, s);
-		
-		
-	}
-	
 	public void testGen() throws Exception {
+		// generation fail cause exeception
 		Builder.generate(BuilderGen.class,new File("./target/test-generated-sources/javacc/"));
 	}
 }
